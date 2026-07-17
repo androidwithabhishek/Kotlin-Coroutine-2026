6a01c8876b518f1c7d224d6fc5caa03cf8caeed0
Abhishek <abhishekgupta766853@gmail.com>
2026-07-18 02:49:14 +0530

    coroutineScope : 18 july

--- src/main/kotlin/coroutine/coroutineBuilders/scoped/coroutineScopeBuilder.kt
+++ src/main/kotlin/coroutine/coroutineBuilders/scoped/coroutineScopeBuilder.kt
@@ -1,4 +1,21 @@
 package coroutine.coroutineBuilders.scoped
 
-class coroutineScopeBuilder {
+import coroutine.coroutineBuilders.standAlon.runBlocking
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(): kotlin.Unit = runBlocking {
+
+
+    coroutineScope {
+
+        launch { println("coroutine scope first") }
+
+
+        launch { println("coroutine scope second") }
+    }
+
+
+
 }
