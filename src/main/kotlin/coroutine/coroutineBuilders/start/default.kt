f996a237ab7dad22789918535b8f4b91e80028f0
Abhishek <abhishekgupta766853@gmail.com>
2026-07-18 02:33:01 +0530

    start 18 july

--- src/main/kotlin/coroutine/coroutineBuilders/start/default.kt
+++ src/main/kotlin/coroutine/coroutineBuilders/start/default.kt
@@ -1,6 +1,8 @@
 package coroutine.coroutineBuilders.start
 
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -11,12 +13,16 @@ import kotlinx.coroutines.runBlocking
 fun main(): kotlin.Unit = runBlocking {
 
 
+    val job = launch(start = CoroutineStart.UNDISPATCHED, context = Dispatchers.IO) {
+        println("Start")
 
-val job =  launch(start = CoroutineStart.LAZY) {
-    println(name())
+        println(Thread.currentThread().name)
+        println(name())
+        println(Thread.currentThread().name)
+        println("End")
+    }
 
-}
-job
+
 
 
 }
